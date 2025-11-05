540d0bed782f997fcdb0ef01d5907b688c0a84b1
ZahaAnass <zahaanass277@gmail.com>
2025-11-05 23:08:23 +0100

    add hero section

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,7 +1,46 @@
+"use client";
+import Image from "next/image";
+import { BsArrowRight } from "react-icons/bs";
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div className="relative h-screen flex items-center justify-center text-white overflow-hidden flex-col">
+            <div className="relative z-10 flex flex-col items-center">
+                <Image 
+                    src="/images/s1.jpg"
+                    alt="Hero image" 
+                    width={150} 
+                    height={150} 
+                    className="rounded-full border-8 border-[#0c0c48aa]"
+                />
+                <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl mt-6 text-center font-bold tracking-wide">
+                    Creating web products,<br /> brands,
+                    <span className="text-cyan-200">
+                        {" "}and experiences.
+                    </span>
+                </h1>
+                <h2 className="mt-5 text-sm text-center px-2 sm:text-2xl font-medium flex items-center">
+                    Hi! I&apos;m Anass - A Passionate
+                    <span className="text-cyan-200 font-bold">
+                        <Typewriter
+                            options={{
+                                strings: ["Frontend Developer", "Backend Developer", "Web Developer"],
+                                autoStart: true,
+                                loop: true,
+                                delay: 75,
+                                deleteSpeed: 50,
+                                wrapperClassName: "pl-2"
+                            }}
+                        />
+                    </span>
+                </h2>
+                <button className="mt-6 px-10 py-4 bg-blue-800 hover:bg-blue-900 transition-all duration-300 cursor-pointer rounded-full text-lg font-medium">
+                    <span>See my work</span>
+                    <BsArrowRight className="h-5 w-5 ml-2 inline-block"/>
+                </button>
+            </div>
+        </div>
     );
 };
 
