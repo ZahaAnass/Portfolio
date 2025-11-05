5c27d522db680cc4cbe85311520260c563c08fd7
ZahaAnass <zahaanass277@gmail.com>
2025-11-05 14:33:54 +0100

    use Responsive nav components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ResponsiveNav from "@/components/Home/NavBar/ResponsiveNav";
 
 const font = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${font.className} antialiased bg-[#0d0d1f]`}>
+        <ResponsiveNav />
         {children}
       </body>
     </html>
