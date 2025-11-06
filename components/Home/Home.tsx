1d7fdbb80ccceb916d23f5b869be8ff14a6b8be6
ZahaAnass <zahaanass277@gmail.com>
2025-11-06 13:06:38 +0100

    add resume section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,4 +1,5 @@
 import Hero from "./Hero/Hero"
+import Resume from "./Resume/Resume"
 import Services from "./Service/Services"
 
 function Home() {
@@ -6,6 +7,7 @@ function Home() {
         <div className="overflow-hidden">
             <Hero />
             <Services />
+            <Resume />
         </div>
     )
 }
