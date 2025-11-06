b6215cc99c2975a3f38092b5e354770cd73e3d38
ZahaAnass <zahaanass277@gmail.com>
2025-11-06 18:01:55 +0100

    add a constant skills

---
+++ constants/skills.ts
@@ -0,0 +1,34 @@
+import { SiJavascript, SiNextdotjs, SiNodedotjs, SiReact, SiTailwindcss, SiTypescript } from "react-icons/si"
+
+export const skills = [
+    {
+        name: "JavaScript",
+        icon: SiJavascript,
+        percentage: 89,
+    },
+    {
+        name: "React Js",
+        icon: SiReact,
+        percentage: 89,
+    },
+    {
+        name: "Next.js",
+        icon: SiNextdotjs,
+        percentage: 90,
+    },
+    {
+        name: "Node.js",
+        icon: SiNodedotjs,
+        percentage: 89,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: SiTailwindcss,
+        percentage: 89,
+    },
+    {
+        name: "TypeScript",
+        icon: SiTypescript,
+        percentage: 89,
+    }
+]
