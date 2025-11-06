7707a6a470c6617e90d3cc423c42a24e453a8caf
ZahaAnass <zahaanass277@gmail.com>
2025-11-06 01:40:09 +0100

    enhance grid diplay

--- components/Home/Service/Services.tsx
+++ components/Home/Service/Services.tsx
@@ -6,7 +6,7 @@ const Services = () => {
             <h1 className='text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white'>
                 Colleborate with brand <br /> and agencies to create <br /> impactful results
             </h1>
-            <div className="w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-20 items-center">
+            <div className="w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mt-20 items-center">
                 <div>
                     <ServiceCard
                         icon="/images/s1.png" 
