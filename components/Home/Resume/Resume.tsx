9b6dfd57e8ecab50aef53b2a0c6e2e2aa9bf4e54
ZahaAnass <zahaanass277@gmail.com>
2025-11-06 13:07:26 +0100

    create resume section

---
+++ components/Home/Resume/Resume.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ResumeCard from './ResumeCard'
+import { FaCodepen, FaDatabase, FaReact } from 'react-icons/fa'
+import { BiBadge } from 'react-icons/bi'
+
+const Resume = () => {
+    return (
+        <div className='pt-20 pb-16'>
+            <h1 className='w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 xl:grid-cols-2 gap-10'>
+                {/* Wrok Part */}
+                <div>
+                    <h1 className='text-3xl sm:text-4xl font-bold text-white'>
+                        My Work <span className='text-cyan-200'>Experience</span>
+                    </h1>
+                    <div className='mt-10'>
+                        <ResumeCard Icon={FaCodepen} role="Full-Stack Developer" date={`${new Date().getFullYear() + " - " + "Present"}`} />
+                        <ResumeCard Icon={FaReact} role="Front-End Developer" />
+                        <ResumeCard Icon={FaDatabase} role="Back-End Developer" />
+                    </div>
+                </div>
+                {/* Education Part */}
+                <div>
+                    <h1 className='text-3xl sm:text-4xl font-bold text-white'>
+                        My <span className='text-cyan-200'>Education</span>
+                    </h1>
+                    <div className='mt-10'>
+                        <ResumeCard Icon={BiBadge} role="Design Istitute of Technology" date={`Jan 2020 - Dec 2021`} />
+                        <ResumeCard Icon={FaReact} role="Bachelor of Computer Science" date={`Jan 2021 - Dec 2023`} />
+                        <ResumeCard Icon={FaDatabase} role="Master of Computer Science" date={`Jan 2023 - Present`} />
+                    </div>
+                </div>
+            </h1>
+        </div>
+    )
+}
+
+export default Resume
