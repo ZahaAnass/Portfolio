775bad158015572bf04c0b489aae1d0d7fbd2734
ZahaAnass <zahaanass277@gmail.com>
2025-11-06 01:32:12 +0100

    add service section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "./Hero/Hero"
+import Services from "./Service/Services"
 
 function Home() {
     return (
         <div className="overflow-hidden">
             <Hero />
+            <Services />
         </div>
     )
 }
