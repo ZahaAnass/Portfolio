3894472e724e6c0bfd8f2e0bfa2750c14bb2ab89
ZahaAnass <zahaanass277@gmail.com>
2025-11-07 15:49:41 +0100

    add footer

---
+++ components/Home/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <footer className="bg-blue-900/20 p-5">
+            <div className="text-lg text-white/70 text-center">
+                <p>Anass Zaha | {new Date().getFullYear() ?? 2026} Portfolio</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
